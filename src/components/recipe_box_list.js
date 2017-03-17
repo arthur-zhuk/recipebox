e096cdaf704b7cb133a84850fa392e5fcbffee69
Arthur Zhuk <arthurzhuk@gmail.com>
2017-03-17 09:47:39 -0700

    not sure what the change was

--- src/components/recipe_box_list.js
+++ src/components/recipe_box_list.js
@@ -5,7 +5,14 @@ import RecipeItem from './recipe_item';
 class RecipeBoxList extends Component {
   render() {
     const recipeList = this.props.recName.map((recipe, id) => {
-      return <RecipeItem updateFunc2={this.props.updateFunc1} deleteItemFunc={this.props.deleteItemFunc} key={id} recName={recipe[0]} recIngs={recipe[1].split(",")} eKey={id + 1} />
+      return <RecipeItem
+				updateFunc2={this.props.updateFunc1}
+				deleteItemFunc={this.props.deleteItemFunc}
+				key={id}
+				recName={recipe[0]}
+				recIngs={recipe[1].split(",")}
+				eKey={id + 1}
+			/>
     });
     return (
       <Well>
